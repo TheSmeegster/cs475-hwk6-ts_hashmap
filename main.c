0a5771dadc20126c40c46927fa510a005f372901
TheSmeegster <yeztiboi@gmail.com>
2023-03-30 09:09:25 +0000

    Final Commit

--- main.c
+++ main.c
@@ -14,7 +14,22 @@ int main(int argc, char *argv[]) {
 	int num_threads = atoi(argv[1]);
 	int capacity = (unsigned int) atoi(argv[2]);
 
+	if(num_threads == 0 || capacity == 0){
+		printf("num threads and hashmap capacity must both be greater than 0");
+		return 1;
+	}
+
+	
 	// TODO: Write your test
+	ts_hashmap_t *map = initmap(capacity);
+	put(map, 5, 2);
+	put(map, 6, 10);
+	put(map, 3, 7);
+	del(map, 5);
+	put(map, 5, 4);
+	printf("%i\n", get(map, 5));
+	printmap(map);
+
 
 	return 0;
 }
--- ts_hashmap.c
+++ ts_hashmap.c
@@ -11,9 +11,26 @@
  * @param capacity initial capacity of the hashmap.
  * @return a pointer to a new thread-safe hashmap.
  */
+
+//Locking mechainism
+ pthread_mutex_t lock;
+
 ts_hashmap_t *initmap(int capacity) {
-  // TODO
-  return NULL;
+  
+  //Creasts a map pointer
+  ts_hashmap_t *map = (ts_hashmap_t*) malloc(sizeof(ts_hashmap_t) * 1);
+
+  //Populates the map with ts_entry_t slots based on the capacity
+  map->table = (ts_entry_t**) malloc(sizeof(ts_entry_t*) * capacity);
+
+  //Initializes entries to NULL
+  for(int i = 0; i < capacity; i++){
+    map->table[i] = NULL;
+  }
+
+  //Sets capacity of this specific map, and returns the ts_hashmap_t
+  map->capacity = capacity;
+  return map;
 }
 
 /**
@@ -22,8 +39,29 @@ ts_hashmap_t *initmap(int capacity) {
  * @param key a key to search
  * @return the value associated with the given key, or INT_MAX if key not found
  */
+
 int get(ts_hashmap_t *map, int key) {
-  // TODO
+
+  //Spinlocks
+  while(pthread_mutex_init(&lock, NULL) != 0){
+    //Spin
+  }
+
+  //Obtains lock
+  pthread_mutex_lock(&lock);
+
+  //Obtains correct index for map
+  int index = key % map->capacity;
+
+  //Accesses current entry
+  ts_entry_t *entry = map->table[index];
+
+  //Returns associated value if the given key exists, otherwise returns INT_MAX
+  if(entry != NULL){
+    pthread_mutex_unlock(&lock);
+    return entry->value;
+  }
+  pthread_mutex_unlock(&lock);
   return INT_MAX;
 }
 
@@ -35,7 +73,43 @@ int get(ts_hashmap_t *map, int key) {
  * @return old associated value, or INT_MAX if the key was new
  */
 int put(ts_hashmap_t *map, int key, int value) {
-  // TODO
+
+  //Spin locks
+  while(pthread_mutex_init(&lock, NULL) != 0){
+    //Spin
+  }
+
+  //Obtains lock
+  pthread_mutex_lock(&lock);
+
+  //Obtains correct index for map
+  int index = key % map->capacity;
+  
+  //Accesses current entry
+  ts_entry_t *entry = map->table[index];  
+
+  //If the entry does not exist, creates a new entry, and populates with new key/value
+  if(entry == NULL){
+    ts_entry_t *newEntry = (ts_entry_t*) malloc(sizeof(ts_entry_t) * 1);
+    newEntry->key = key;
+    newEntry->value = value;
+    map->table[index] = newEntry;
+  }
+
+  //Iterates through values until a null value is located, searching for given key
+  while(entry != NULL){
+    if(entry->key == key){
+      int oldVal = entry->value;
+      int* val = (int*) malloc(sizeof(int) * 1);
+      free(entry);
+      
+      entry->value = *val;
+      pthread_mutex_unlock(&lock);
+      return oldVal;
+    }
+    entry = entry->next;
+  }
+  pthread_mutex_unlock(&lock);
   return INT_MAX;
 }
 
@@ -46,7 +120,29 @@ int put(ts_hashmap_t *map, int key, int value) {
  * @return the value associated with the given key, or INT_MAX if key not found
  */
 int del(ts_hashmap_t *map, int key) {
-  // TODO
+
+  //Spin locks
+  while(pthread_mutex_init(&lock, NULL) != 0){
+    //Spin
+  }
+
+  pthread_mutex_lock(&lock);
+
+  //Obtains correct index for map
+  int index = key % map->capacity;
+
+  //Accesses current entry
+  ts_entry_t *entry = map->table[index];
+
+  //If there is a value assiciated with key, removes entry and returns old value
+  if(entry != NULL){
+    int oldVal = entry->value;
+    map->table[index] = NULL;
+    free(entry);
+    pthread_mutex_unlock(&lock);
+    return oldVal;
+  }
+  pthread_mutex_unlock(&lock);
   return INT_MAX;
 }
 
